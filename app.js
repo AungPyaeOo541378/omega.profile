03c82b003343868908f2d3a1840e6b90721a4d74
AungPyaeOo541378 <aungpyaeoo.541378@gmail.com>
2025-05-17 17:31:15 +0900

    commit

--- app.js
+++ app.js
@@ -5,10 +5,53 @@ $(document).on("click", ".nav-input", function () {
     $(this).addClass("bg-gray-900");
   });
   
-  $(document).on("click", "#profile-img", function () {
-    if ($("#profile-icon").hasClass("hidden")) {
-      $("#profile-icon").removeClass("hidden");
-    } else {
-      $("#profile-icon").addClass("hidden");
-    }
+//   $(document).on("click", "#profile-img", function () {
+//     if ($("#profile-icon").hasClass("hidden")) {
+//       $("#profile-icon").removeClass("hidden");
+//     } else {
+//       $("#profile-icon").addClass("hidden");
+//     }
+//   });
+
+$(document).ready(function () {
+    setTimeout(() => {
+      $(".loading-page").addClass("hidden");
+      $(".main-page").removeClass("hidden");
+    }, 3000);
+  });
+
+$(document).on("click", "#home", function () {
+    $(".home-menu").each(function () {
+      $(this).removeClass("hidden");
+    });
+    $(".project-menu").each(function () {
+      $(this).addClass("hidden");
+    });
+    $(".resume-menu").each(function () {
+      $(this).addClass("hidden");
+    });
+  });
+
+$(document).on("click", "#resume", function () {
+    $(".home-menu").each(function () {
+      $(this).addClass("hidden");
+    });
+    $(".project-menu").each(function () {
+      $(this).addClass("hidden");
+    });
+    $(".resume-menu").each(function () {
+      $(this).removeClass("hidden");
+    });
+  });
+
+$(document).on("click", "#projects", function () {
+    $(".home-menu").each(function () {
+      $(this).addClass("hidden");
+    });
+    $(".project-menu").each(function () {
+      $(this).removeClass("hidden");
+    });
+    $(".resume-menu").each(function () {
+      $(this).addClass("hidden");
+    });
   });
