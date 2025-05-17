738df82ceb327a2d6094db0645be50d79fa00ec0
AungPyaeOo541378 <aungpyaeoo.541378@gmail.com>
2025-05-17 15:19:34 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).on("click", ".nav-input", function () {
+    $(".nav-input").each(function () {
+      $(this).removeClass("bg-gray-900");
+    });
+    $(this).addClass("bg-gray-900");
+  });
+  
+  $(document).on("click", "#profile-img", function () {
+    if ($("#profile-icon").hasClass("hidden")) {
+      $("#profile-icon").removeClass("hidden");
+    } else {
+      $("#profile-icon").addClass("hidden");
+    }
+  });
